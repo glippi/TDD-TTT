778c4e5e507fa5f4a818aaf022c507a3e88406d8
Gabriele Lippi <gabriele@lippi.net>
2019-05-07 12:40:45 +0200

    Make tests passes - create cells

--- src/Grid.js
+++ src/Grid.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react'
 export class Grid extends Component {
   render() {
     return (
-      <div className={`grid-wrapper`}>I'm a grid</div>
+      <div> 
+        <div className={`grid-wrapper`}>I'm a grid</div>
+        <div>
+          <div className={`cell`}>1</div>
+          <div className={`cell`}>2</div>
+          <div className={`cell`}>3</div>
+          <div className={`cell`}>4</div>
+          <div className={`cell`}>5</div>
+          <div className={`cell`}>6</div>
+          <div className={`cell`}>7</div>
+          <div className={`cell`}>8</div>
+          <div className={`cell`}>9</div>
+        </div>
+      </div>
     )
   }
 } 
