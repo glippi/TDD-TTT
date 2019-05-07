b5cb3a1b15c4d713e75b066b57507b77a99ced07
Gabriele Lippi <gabriele@lippi.net>
2019-05-07 13:10:23 +0200

    Add dummy implementation for signing the board

--- src/Cell.js
+++ src/Cell.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react' 
 
 export class Cell extends Component {
+  state = {
+    sign: ''
+  }
+
+  signCell = () => {
+    this.setState({ sign: "X" })
+  }
+
   render() {
     const { cellPosition } = this.props
 
     return (
-      <div className={`cell`}>{cellPosition}</div>
+      <div className={`cell`} onClick={this.signCell}>{this.state.sign === '' ? cellPosition : this.state.sign}</div>
     )
   }
 } 
