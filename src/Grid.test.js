15e17f5ca59f2833a8dfd545f9d595af85fb69c4
Gabriele Lippi <gabriele@lippi.net>
2019-05-07 12:33:49 +0200

    Create failing test for cells

--- src/Grid.test.js
+++ src/Grid.test.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils';
 import { Grid } from './Grid'
 
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+
 it('renders Grid element', () => {
-  const div = document.createElement('div');
-  render(<Grid />, div);
-  unmountComponentAtNode(div);
+  render(<Grid />, container);
+  unmountComponentAtNode(container);
+});
+
+
+it('Grid should have 9 divs', () => {
+  act(() => {
+    render(<Grid />, container);
+  });
+  const cells = container.querySelectorAll('.cell');
+  console.log({ cells })
+  expect(cells).toEqual(9);
 });
