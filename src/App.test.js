0aac0283d0f9ad483fa53afef20707d8ea07d133
Gabriele Lippi <gabriele@lippi.net>
2019-05-07 12:17:56 +0200

    Failing test for Grid inside App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { act } from 'react-dom/test-utils';
+import { Grid } from './Grid'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the Grid inside the App', () => {
+  const div = document.createElement('div');
+  act(() => {
+    ReactDOM.render(<App />, div);
+  });
+  const grid = container.querySelector('.grid-wrapper');
+  expect(grid.textContent).toBe('I\'m a grid');
 });
