7f8ba6527828ecde427f3b103f3b6bb589a8082a
Gabriele Lippi <gabriele@lippi.net>
2019-05-07 16:58:19 +0200

    Use Cell component

--- src/Grid.js
+++ src/Grid.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react' 
-import {Cell} from './Cell'
+import { Cell } from './Cell'
+
+const RANGE = [...Array(9).keys()];
 
 export class Grid extends Component {
   render() {
     return (
-      <div> 
-        <div className={`grid-wrapper`}>I'm a grid</div>
-        <div>
-          <div className={`cell`}>1</div>
-          <div className={`cell`}>2</div>
-          <div className={`cell`}>3</div>
-          <div className={`cell`}>4</div>
-          <div className={`cell`}>5</div>
-          <div className={`cell`}>6</div>
-          <div className={`cell`}>7</div>
-          <div className={`cell`}>8</div>
-          <div className={`cell`}>9</div>
-        </div>
-        <Cell cellPosition="1" />
+      <div className="center w-25"> 
+        <h1 className={`grid-wrapper`}>Tic Tac Toe</h1>
+        <div className="flex flex-row flex-wrap justify-center items-centerr">
+        {RANGE.map(n => (
+          <div key={n}> 
+            <Cell cellPosition={n + 1} />
+          </div>
+        ))}
       </div>
+     </div>
     )
   }
 } 
