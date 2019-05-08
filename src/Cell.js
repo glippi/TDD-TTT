004f9534bc1dc37e4842df26070ebee459f56c9d
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 18:19:21 +0200

    Refactor Cell component

--- src/Cell.js
+++ src/Cell.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react'
 
 export class Cell extends Component {
-  state = {
-    sign: ''
-  }
-
-  signCell = () => {
-    this.setState({ sign: "X" })
-  }
-
   render() {
-    const { cellPosition } = this.props
-    const { sign } = this.state
-
-    const text = sign || cellPosition
+    const { cellValue, signBoard } = this.props
 
     return (
-      <div className={`cell flex flex-row justify-center items-center w4 h4 ba pointer`} onClick={this.signCell}>
-        <span className={``}>{text}</span>
+      <div className={`cell flex flex-row justify-center items-center w4 h4 ba pointer`} onClick={()=> signBoard()}>
+        {cellValue}
       </div>
     )
   }
