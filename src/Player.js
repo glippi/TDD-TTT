0eace6fc3396d28331bf8a1de6b0377cea71ba03
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 10:18:45 +0200

    Make tests passes adding state

--- src/Player.js
+++ src/Player.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 export class Player extends Component {
   render() {
+    const { whoIsPlaying } = this.props
+    const actualPlayer = whoIsPlaying === 'player1' ? "Player 1" : "Player 2"
+
     return (
-      <div className={``}>
-        <div className={`player1`}>Player 1 moves</div>
-      </div>
+      <div className={`player`}>{`${actualPlayer} moves`}</div>
     )
   }
 }
