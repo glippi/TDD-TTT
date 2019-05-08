cee7cb75a8a5e1e6485bb481a3a95bcfec62647f
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 09:47:22 +0200

    Make tests passes

--- src/Player.js
+++ src/Player.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export class Player extends Component {
   render() {
     return (
-      <div className={``}>a</div>
+      <div className={``}>
+        <div className={`player1`}>Player 1 moves</div>
+      </div>
     )
   }
 }
