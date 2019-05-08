dada67417a3c829d405c85a28a3a3b7af24b73ed
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 09:53:52 +0200

    Add Player component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Grid } from './Grid'
+import { Player } from './Player'
 
 function App() {
   return (
     <div className="App">
+      <Player />
       <Grid />
     </div>
   );
