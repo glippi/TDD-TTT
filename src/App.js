be40502c8afe1c3175831ccf30cf24fb276e475d
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 10:11:52 +0200

    Make tests passes, creating minimum state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Grid } from './Grid'
 import { Player } from './Player'
 
 export default class App extends Component {
+  state = {}
+
   render() {
     return (
     <div className="App flex flex-row items-center justify-center">
