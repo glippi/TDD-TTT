fc5b3ed5da1a53e8bea3fffbaaf5dbb3eeb6f982
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 11:40:22 +0200

    Refactor test passing grid state

--- src/Grid.test.js
+++ src/Grid.test.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
+import { create } from "react-test-renderer"
 import { act } from 'react-dom/test-utils'
 import { Grid } from './Grid'
+import App from './App'
 
 let container;
+let grid;
 
 beforeEach(() => {
   container = document.createElement('div');
   document.body.appendChild(container);
+  const app = create(<App />)
+  const instance = app.getInstance();
+  grid = instance.state.grid
 });
 
 afterEach(() => {
@@ -18,14 +24,14 @@ afterEach(() => {
 
 describe('Grid component should', () => {
   test('render without crashing', () => {
-    render(<Grid />, container);
+    render(<Grid grid={grid} />, container);
 
     unmountComponentAtNode(container);
   });
 
   test('render 9 cells', () => {
     act(() => {
-      render(<Grid />, container)
+      render(<Grid grid={grid} />, container)
     });
 
     const cells = container.querySelectorAll('.cell');
