db745c02bebd336e87307d371ce7319dae6e34e2
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 18:19:53 +0200

    Modify component to reflect changes in App

--- src/Grid.js
+++ src/Grid.js
@@ -3,15 +3,15 @@ import { Cell } from './Cell'
 
 export class Grid extends Component {
   render() {
-    const { grid, changePlayer } = this.props
+    const { grid, signBoard } = this.props
 
     return (
       <div className="w-25"> 
         <h1 className={`grid-wrapper`}>Tic Tac Toe</h1>
         <div className="flex flex-row flex-wrap justify-center items-centerr">
-        {grid.map(n => (
-          <div key={n}> 
-            <Cell cellPosition={n} changePlayer={changePlayer} />
+        {grid.map((value, index) => (
+          <div key={index}> 
+            <Cell cellValue={value} signBoard={signBoard(index + 1)} />
           </div>
         ))}
       </div>
