b7c30bed865cce44e51a4801bb0c0e14123c2bf8
Gabriele Lippi <gabriele@lippi.net>
2019-05-08 11:40:32 +0200

    Create grid state

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import { Player } from './Player'
 
 export default class App extends Component {
   state = {
+    grid: [1,2,3,4,5,6,7,8,9],
     actualPlayer: "player1"
   }
 
-  render() {
+  changePlayer = () => {
     const { actualPlayer } = this.state
+    const nextPlayer = actualPlayer === "player1" ? "player2" : "player1"
+    this.setState({ actualPlayer: nextPlayer })
+  }
+
+  render() {
+    const { grid, actualPlayer } = this.state
 
     return (
     <div className="App flex flex-row items-center justify-center">
       <Player whoIsPlaying={actualPlayer} />
-      <Grid />
+      <Grid grid={grid} changePlayer={this.changePlayer} />
     </div>
     )
   }
