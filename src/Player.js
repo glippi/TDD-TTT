8e6cf5f689a753a068d3b867031712ac857db27d
Gabriele Lippi <gabriele@lippi.net>
2019-05-10 00:47:34 +0200

    Centering approx the player display

--- src/Player.js
+++ src/Player.js
@@ -6,7 +6,7 @@ export class Player extends Component {
     const actualPlayer = whoIsPlaying === 'player1' ? "Player 1" : "Player 2"
 
     return (
-      <h2 className={`player ${whoIsPlaying === 'player1' ? "blue" : "red"} mv5 f3 fw4 ttu tracked`}>
+      <h2 className={`player ${whoIsPlaying === 'player1' ? "blue" : "red"} ml5 mv5 f3 fw4 ttu tracked`}>
         {`${actualPlayer} moves`}
       </h2>
     )
