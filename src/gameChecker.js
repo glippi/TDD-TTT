71b038ee40a1b526d9e4f84beb37e2bc3b95f62e
Gabriele Lippi <gabriele@lippi.net>
2019-05-09 10:22:51 +0200

    Make test passes for third horiz row

--- src/gameChecker.js
+++ src/gameChecker.js
@@ -1,10 +1,13 @@
 const firstHorizontalRow = (grid, sign) => grid[0] === sign && grid[1] === sign && grid[2] === sign
 const secondHorizontalRow = (grid, sign) => grid[3] === sign && grid[4] === sign && grid[5] === sign
+const thirdHorizontalRow = (grid, sign) => grid[6] === sign && grid[7] === sign && grid[8] === sign
 
 export default function gameChecker(grid) {
   if (firstHorizontalRow(grid, "X")
     || 
    secondHorizontalRow(grid, "X")
+    || 
+   thirdHorizontalRow(grid, "X")
   ) {
     return "player1"
   }
