7a0a3745d325073cfea577501ba3149c66e1a5b9
Gabriele Lippi <gabriele@lippi.net>
2019-05-09 10:27:22 +0200

    Add tests for second vertical row

--- src/gameChecker.test.js
+++ src/gameChecker.test.js
@@ -8,4 +8,36 @@ describe('gameChecker should verify who is the winner', ()=>{
 
     expect(winner).toBe("player1");
   })
+
+  test('player1 wins with second horizontal row', ()=> {
+    const grid = ["X","O", "O", "X", "X", "X", "O", "8", "9" ];
+
+    const winner = gameChecker(grid);
+
+    expect(winner).toBe("player1");
+  })
+
+  test('player1 wins with third horizontal row', ()=> {
+    const grid = ["1","2", "O", "4", "O", "6", "X", "X", "X" ];
+
+    const winner = gameChecker(grid);
+
+    expect(winner).toBe("player1");
+  })
+
+  test('player1 wins with first vertical row', ()=> {
+    const grid = ["X","X", "X", "O", "O", "6", "7", "8", "9" ];
+
+    const winner = gameChecker(grid);
+
+    expect(winner).toBe("player1");
+  })
+
+  test('player1 wins with second vertical row', ()=> {
+    const grid = ["O","X", "3", "X", "O", "X", "7", "8", "9" ];
+
+    const winner = gameChecker(grid);
+
+    expect(winner).toBe("player1");
+  })
 })
