d89a635f3d6edcd5fadd2e08eff5f0d7a204a168
Gabriele Lippi <gabriele@lippi.net>
2019-05-09 23:52:30 +0200

    Restyling Player

--- src/Player.js
+++ src/Player.js
@@ -6,7 +6,7 @@ export class Player extends Component {
     const actualPlayer = whoIsPlaying === 'player1' ? "Player 1" : "Player 2"
 
     return (
-      <div className={`player ${whoIsPlaying === 'player1' ? "blue" : "red"}`}>{`${actualPlayer} moves`}</div>
+        <h2 className={`player ${whoIsPlaying === 'player1' ? "blue" : "red"} mv5 f3 fw4 ttu tracked`}>{`${actualPlayer} moves`}</h2>
     )
   }
 }
