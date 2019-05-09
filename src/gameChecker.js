3cfca1dfebd7db889f68ecfadd7e2dc5e607ebd6
Gabriele Lippi <gabriele@lippi.net>
2019-05-09 10:25:19 +0200

    Add first vertical row

--- src/gameChecker.js
+++ src/gameChecker.js
@@ -2,12 +2,16 @@ const firstHorizontalRow = (grid, sign) => grid[0] === sign && grid[1] === sign 
 const secondHorizontalRow = (grid, sign) => grid[3] === sign && grid[4] === sign && grid[5] === sign
 const thirdHorizontalRow = (grid, sign) => grid[6] === sign && grid[7] === sign && grid[8] === sign
 
+const firstVerticalRow = (grid, sign) => grid[0] === sign && grid[3] === sign && grid[6] === sign
+
 export default function gameChecker(grid) {
   if (firstHorizontalRow(grid, "X")
     || 
    secondHorizontalRow(grid, "X")
     || 
    thirdHorizontalRow(grid, "X")
+    || 
+   firstVerticalRow(grid, "X")
   ) {
     return "player1"
   }
