87960d302db9a95265303453f619a65490565707
Gabriele Lippi <gabriele@lippi.net>
2019-05-10 10:59:22 +0200

    Small refactor of styles

--- src/Player.js
+++ src/Player.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
 
 export class Player extends Component {
   render() {
-    const { whoIsPlaying } = this.props
+    const { whoIsPlaying, winner } = this.props
     const actualPlayer = whoIsPlaying === 'player1' ? "Player 1" : "Player 2"
 
     return (
-      <h2 className={`player ${whoIsPlaying === 'player1' ? "blue" : "red"} ml5 mv5 f3 fw4 ttu tracked`}>
-        {`${actualPlayer} moves`}
+      <h2 className={`player ${whoIsPlaying === 'player1' ? "blue" : "red"} mr3 mv5 f3 fw4 ttu tracked`}>
+        {`${winner ? `${winner} wins!` : `${actualPlayer} moves`}`}
+        {winner &&
+          (<div className={`pointer black absolute mv1 f5`}>
+            Restart the game?
+            <div className="mv1">&#128260;</div>
+          </div>)
+        }
       </h2>
     )
   }
