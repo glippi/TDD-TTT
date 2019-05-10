fdd229895270b31a6fd0ad63b4d4fa8924221a5e
glippi <gabriele@lippi.net>
2019-05-10 10:00:35 +0000

    Run production build

---
+++ precache-manifest.0a82e3f4eb72facbb9213eb25ea45fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33689ecf7a80c4b2a895536f64a4b747",
+    "url": "/TDD-TTT/index.html"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/TDD-TTT/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a963ae0605418c5cefc4",
+    "url": "/TDD-TTT/static/css/main.586eff90.chunk.css"
+  },
+  {
+    "revision": "2883d945f805b12f1a47",
+    "url": "/TDD-TTT/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "a963ae0605418c5cefc4",
+    "url": "/TDD-TTT/static/js/main.5252927a.chunk.js"
+  },
+  {
+    "revision": "a5725d625de21fe8fe54",
+    "url": "/TDD-TTT/static/js/runtime~main.20239f14.js"
+  }
+]);
