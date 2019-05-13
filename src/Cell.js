0c173e64e5fc868dcc6ef7d6789b14284dda5c13
Gabriele Lippi <gabriele@lippi.net>
2019-05-13 10:36:04 +0200

    Prevent from selecting two times the same cell

--- src/Cell.js
+++ src/Cell.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export class Cell extends Component {
   render() {
     const { cellValue, signBoard } = this.props
+    const clickable = cellValue !== '❌' && cellValue !== '⭕'
 
     return (
-      <div className={`cell flex flex-row justify-center items-center w4 h4 ba pointer`} onClick={()=> signBoard()}>
+      <div className={`cell flex flex-row justify-center items-center w4 h4 ba ${clickable ? 'pointer' : 'pointer-events-none'}`} onClick={()=> signBoard()}>
         {cellValue}
       </div>
     )
